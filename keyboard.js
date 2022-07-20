bc71742e462070750dba8d016d7b440aa6f38a03
Breck Yunits <breck@Brecks-MacBook-Pro.local>
2022-07-20 10:54:10 -1000

    pldb vis

--- keyboard.js
+++ keyboard.js
@@ -1,109 +1,164 @@
-const jQuery = $
+const jQuery = $;
+
+const langs = TreeNode.fromCsv(`title,id,appeared,rank
+A+,aplus,1988,876
+B,b,1969,242
+C,c,1972,2
+D,d,2001,48
+E,e,1997,229
+F#,f-sharp,2005,53
+G,labview,1986,92
+H,h-lang,2019,3232
+I,i,2012,2097
+J,j,1990,131
+K,k,1993,214
+L,l,2013,3404
+M,mumps,1966,96
+N,n,1990,3636
+O,o,1986,1952
+P,p,2013,786
+Q,q,2003,268
+R,r,1993,10
+S,s,1976,344
+T,t,1980,567
+U,u,2013,3307
+V,v,2019,161
+W,w,2019,3233
+X,x,2021,3124
+Y,y,1981,1344
+Z,z,2019,3105`);
+
+const theMap = {};
+langs.forEach((item) => {
+  theMap[item.get("title").toLowerCase().substr(0, 1)] = item;
+});
 
 class Symbol {
   constructor(symbolElement) {
-    this._jQueryElement = symbolElement
-    let id = symbolElement.attr("value") || symbolElement.text().toLowerCase() + "Symbol"
+    this._jQueryElement = symbolElement;
+    let id =
+      symbolElement.attr("value") ||
+      symbolElement.text().toLowerCase() + "Symbol";
 
-    this._initData(id)
-    this._age = 2020 - this._year
-    this._id = id
-    this._original = symbolElement.text()
+    this._initData(id);
+    this._age = 2020 - this._year;
+    this._id = id;
+    this._original = symbolElement.text();
   }
   replaceWithAge() {
-    this._jQueryElement.text(this._age)
-    this._state = "age"
+    this._jQueryElement.text(this._age);
+    this._state = "age";
+  }
+  pLangs() {
+    const text = this._jQueryElement.text().toLowerCase();
+    const hit = theMap[text];
+    if (!hit) this.hide();
+    else {
+      const year = hit.get("appeared");
+      const rank = hit.get("rank");
+      this._jQueryElement.html(
+        `<span class="tiny">&nbsp;</span>` +
+          hit.get("title") +
+          "<span class='tiny tiny2'>#" +
+          `${rank}</span>`
+      );
+      this._state = "age";
+    }
   }
   hideIfOlderThan(value) {
-    if (this._age >= parseInt(value)) this.hide()
-    else this.show()
+    if (this._age >= parseInt(value)) this.hide();
+    else this.show();
   }
   hideIfYoungerThan(value) {
-    if (this._age <= parseInt(value)) this.hide()
-    else this.show()
+    if (this._age <= parseInt(value)) this.hide();
+    else this.show();
   }
   hide() {
-    this._jQueryElement.css("opacity", "0")
-    const sib = this.getSibling()
-    if (!sib || (sib && sib.isHidden())) this._jQueryElement.parent().css("opacity", "0")
-    this._hidden = true
+    this._jQueryElement.css("opacity", "0");
+    const sib = this.getSibling();
+    if (!sib || (sib && sib.isHidden()))
+      this._jQueryElement.parent().css("opacity", "0");
+    this._hidden = true;
   }
   getSibling() {
     // todo: cleanup
-    const sib = this._jQueryElement.siblings()[0]
-    if (!sib) return undefined
-    const index = parseInt(jQuery(sib).attr("symbol-index"))
-    return symbols[index]
+    const sib = this._jQueryElement.siblings()[0];
+    if (!sib) return undefined;
+    const index = parseInt(jQuery(sib).attr("symbol-index"));
+    return symbols[index];
   }
   isHidden() {
-    return this._hidden === true
+    return this._hidden === true;
   }
   show() {
-    this._jQueryElement.css("opacity", "")
-    this._jQueryElement.parent().css("opacity", "")
-    this._hidden = false
+    this._jQueryElement.css("opacity", "");
+    this._jQueryElement.parent().css("opacity", "");
+    this._hidden = false;
   }
   replaceWithYear() {
-    const suffix = this._year < 0 ? "BC" : ""
-    this._jQueryElement.text(Math.abs(this._year) + suffix)
-    this._state = "year"
+    const suffix = this._year < 0 ? "BC" : "";
+    this._jQueryElement.text(Math.abs(this._year) + suffix);
+    this._state = "year";
   }
   replaceWithOriginal() {
-    this._jQueryElement.text(this._original)
-    this._state = "original"
+    this._jQueryElement.text(this._original);
+    this._state = "original";
   }
   getUrl() {
-    return this._url
+    return this._url;
   }
   _initData(char) {
     // TildaSymbol 1086 https://en.wikipedia.org/wiki/Tilde
     const hit = keyboardTree
       .toString()
       .split("\n")
-      .find(line => line.trim().startsWith(char))
+      .find((line) => line.trim().startsWith(char));
     if (hit) {
-      const words = hit.trim().split(" ")
-      this._url = words[2]
-      this._year = parseInt(words[1].replace("~", ""))
+      const words = hit.trim().split(" ");
+      this._url = words[2];
+      this._year = parseInt(words[1].replace("~", ""));
     }
   }
 }
 
-const symbols = []
+const symbols = [];
 const main = async () => {
-  jQuery(".key").each(function(index, el) {
+  jQuery(".key").each(function (index, el) {
     jQuery(this)
       .find("span,b")
-      .each(function(symbol) {
-        jQuery(this).attr("symbol-index", symbols.length)
-        symbols.push(new Symbol(jQuery(this)))
-      })
-  })
+      .each(function (symbol) {
+        jQuery(this).attr("symbol-index", symbols.length);
+        const sym = new Symbol(jQuery(this));
+        symbols.push(sym);
+        sym.pLangs();
+      });
+  });
+
   // If someone clicks on a key but not a span or b, click the first child.
   // Ideally we'd just refactor the HTML and change spans and bs to a tags, but
   // no time for that now :)
-  jQuery(".key").on("click", function() {
-    const firstHit = jQuery(this).find("span,b")[0]
-    if (firstHit) jQuery(firstHit).click()
-  })
-  jQuery(".key span,.key b").on("click", function() {
-    const index = parseInt(jQuery(this).attr("symbol-index"))
-    const sym = symbols[index]
-    window.location = sym.getUrl()
-    return false
-  })
-  jQuery("#controls a").on("click", function() {
-    const command = jQuery(this).attr("value")
-    const argIfAny = jQuery(this).attr("data-arg")
-    symbols.forEach(sym => sym[command](argIfAny))
-  })
-  jQuery("#slider").on("input", function() {
-    const year = parseInt(jQuery(this).val())
-    const val = 4020 - year
-    symbols.forEach(sym => sym.hideIfYoungerThan(val))
-    jQuery("#sliderOutput").text(year - 2000)
-  })
+  jQuery(".key").on("click", function () {
+    const firstHit = jQuery(this).find("span,b")[0];
+    if (firstHit) jQuery(firstHit).click();
+  });
+  jQuery(".key span,.key b").on("click", function () {
+    const index = parseInt(jQuery(this).attr("symbol-index"));
+    const sym = symbols[index];
+    window.location = sym.getUrl();
+    return false;
+  });
+  jQuery("#controls a").on("click", function () {
+    const command = jQuery(this).attr("value");
+    const argIfAny = jQuery(this).attr("data-arg");
+    symbols.forEach((sym) => sym[command](argIfAny));
+  });
+  jQuery("#slider").on("input", function () {
+    const year = parseInt(jQuery(this).val());
+    const val = 4020 - year;
+    symbols.forEach((sym) => sym.hideIfYoungerThan(val));
+    jQuery("#sliderOutput").text(year - 2000);
+  });
   //symbols.forEach(sym => sym.replaceWithAge())
-}
+};
 
-jQuery(document).ready(main)
+jQuery(document).ready(main);
